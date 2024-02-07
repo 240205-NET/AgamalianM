f6797bafa9d17a6c27e4d115124e0ff9c3f7a0e2
M3GAalien <agamalianmike@gmail.com>
2024-02-07 13:11:36 -0500

    added new account type SavingsAccount and added ability to see account transactions

--- FirstNet/banking/Program.cs
+++ FirstNet/banking/Program.cs
@@ -2,7 +2,19 @@
     class Program{
         public static void Main(string[] args){
             
-            
+            Account account2 = new SavingsAccount("James", 140.43);
+            Account account1 = new SavingsAccount("Michael", 100.13);
+            Account account3 = new SavingsAccount("John", 10.23);
+
+            Console.WriteLine(account1.DisplayBalance());
+            Console.WriteLine(account2.DisplayBalance());
+            Console.WriteLine(account3.DisplayBalance());
+
+            account2.MakeDeposit(20.3, "For funsies");
+            Console.WriteLine(account2.DisplayTransactionHistory());
+            account1.MakeWithdrawl(10.50 , "NEED COFFEE");
+            Console.WriteLine(account1.DisplayTransactionHistory());
+
         }
     }
 }
--- FirstNet/banking/account.cs
+++ FirstNet/banking/account.cs
@@ -1,27 +1,58 @@
 namespace Banking{
-    class Account
+    abstract class Account
     {
         //fields
         public int accountNumber {get;set;}
-
         protected double balance;
-
         private string owner;
-
         private static int accountNumberSeed = 1;
+        private List<Transaction> transactions = new List<Transaction>();
         
         //constructor
         public Account(string ownerName, double initialBalance){
             this.accountNumber = accountNumberSeed++;
             
             this.owner = ownerName;
-            makeDeposit(initialBalance);
+            MakeDeposit(initialBalance);
         }
 
-
-
-        
         //methods
+        public virtual void MakeDeposit(double amount, string note = ""){
+            if(amount < 0){
+                Console.WriteLine("Deposit can not be less than 0");
+            }else{
+                balance += amount;
+                Transaction deposit = new Transaction(amount, DateTime.Now, note);
+                transactions.Add(deposit); 
+            }
+        }
+
+        public virtual void MakeWithdrawl(double amount, string note = ""){
+            if(amount < 0){
+                Console.WriteLine("Withdrawl can not be less than 0");
+            } else if(balance - amount < 0){
+                Console.WriteLine("Insufficient funds!");
+            }else{
+                balance -= amount;
+                Transaction withdrawl = new Transaction(-amount, DateTime.Now, note = "");
+                transactions.Add(withdrawl);
+            }
+        }
+
+        public virtual string DisplayBalance(){
+            return "Account #" + this.accountNumber + " has a balance of: " + this.balance;
+        }
+
+        public virtual string DisplayTransactionHistory(){
+            var history = new System.Text.StringBuilder();
+
+            history.AppendLine("Date\t\tAmount\t\tNote");
+            foreach(Transaction item in transactions){
+                history.AppendLine($"{item.date.ToShortDateString()}\t{item.amount}\t\t{item.note}");
+            }
+            history.AppendLine("Current Balance:\t\t" + balance);
+            return history.ToString();
+        }
     }
 }
 
