7af3651270a94acfd8c1e2891488961b136747d4
M3GAalien <agamalianmike@gmail.com>
2024-02-12 15:57:04 -0500

    split project into .app and .logic namespaces

---
+++ School/School.App/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace School.App
+{
+    class Program
+    {
+        static void Main()
+        // To Do Monday:
+        // - review the changes (person, student, teacher, course, class, program)
+        // - separate concerns (.App/.Logic)
+        // - XML commenting
+        // - retrieve specific person
+        {
+            try
+            {
+                Console.WriteLine("School Starting...");
+
+                School MySchool = new School();
+                // Student tmp = MySchool.GetStudent();
+                // Console.WriteLine(tmp.name);
+                Console.WriteLine(MySchool.GetStudentsInfo());
+                Console.WriteLine(MySchool.GetTeachersInfo());
+
+                Console.WriteLine("School Ending...");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
